1d31802cb243ba81ebe798165bb2bb193c502c44
Ethan Harsh <ethan@ethanharsh.com>
2022-03-13 18:29:43 +0000

    display data

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,11 @@
 const { defineConfig } = require('@vue/cli-service')
+const { ProvidePlugin } = require('webpack')
 module.exports = defineConfig({
   transpileDependencies: true,
   configureWebpack: {
     resolve: {
       fallback: {
+          "buffer": require.resolve('buffer/'),
           "assert": require.resolve('assert'),
           "http": require.resolve('stream-http'),
           "https": require.resolve('https-browserify'),
@@ -12,6 +14,14 @@ module.exports = defineConfig({
           "stream": require.resolve('stream-browserify'),
           "crypto": require.resolve('crypto-browserify'),
       }       
-    }
+    },
+    plugins: [
+        new ProvidePlugin({
+            Buffer: ['buffer', 'Buffer'],
+        }),
+        new ProvidePlugin({
+            process: 'process/browser',
+        })
+    ]
   }
 })
