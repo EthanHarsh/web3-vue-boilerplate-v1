5e4f06e514dd867b59895a8e28fd5c2c4beaf46f
Ethan Harsh <ethan@ethanharsh.com>
2022-03-13 16:42:59 +0000

    init

---
+++ src/components/MainApp.vue
@@ -0,0 +1,16 @@
+<template>
+    <hello-metamask />
+</template>
+
+<script>
+    import HelloMetamask from '@/components/HelloMetaMask.vue';
+    export default {
+        name: 'Main-App',
+        components: {
+            'hello-metamask': HelloMetamask
+        }
+    }
+</script>
+
+<style scoped>
+</style>
