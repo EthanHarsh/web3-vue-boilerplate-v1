6c113374e157b0cb316cce9f66d3a287ef87841d
Ethan Harsh <ethan@ethanharsh.com>
2022-03-13 17:36:49 +0000

    web3 to store

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,16 +1,19 @@
 <template>
-    <hello-metamask />
+  <hello-metamask />
 </template>
 
 <script>
-    import HelloMetamask from '@/components/HelloMetaMask.vue';
-    export default {
-        name: 'Main-App',
-        components: {
-            'hello-metamask': HelloMetamask
-        }
-    }
+import HelloMetamask from "@/components/HelloMetaMask.vue";
+export default {
+  name: "Main-App",
+  beforeCreate() {
+    console.log("registerWeb3 Action dispatched from MainApp.vue");
+    this.$store.dispatch("registerWeb3");
+  },
+  components: {
+    "hello-metamask": HelloMetamask,
+  },
+};
 </script>
 
-<style scoped>
-</style>
+<style scoped></style>
