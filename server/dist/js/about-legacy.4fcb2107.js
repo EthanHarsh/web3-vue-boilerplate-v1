e9b2f10ed25db865d63237af22375dfbbc594f79
Ethan Harsh <ethan@ethanharsh.com>
2022-03-15 05:39:11 +0000

    remove dist

--- server/dist/js/about-legacy.4fcb2107.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self["webpackChunkweb3_vue_boilerplate_v1"]=self["webpackChunkweb3_vue_boilerplate_v1"]||[]).push([[443],{15830:function(e,t,n){n.r(t),n.d(t,{default:function(){return i}});var u=function(){var e=this,t=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,t=e.$createElement,n=e._self._c||t;return n("div",{staticClass:"about"},[n("h1",[e._v("This is an about page")])])}],a=n(1001),r={},s=(0,a.Z)(r,u,l,!1,null,null,null),i=s.exports}}]);
-//# sourceMappingURL=about-legacy.4fcb2107.js.map
